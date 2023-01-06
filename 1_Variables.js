8b5b9061794af0654c03f930dadd7c5f27d009ac
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2023-01-06 23:47:09 +0530

    Create 1_Variables.js

---
+++ 1_Variables.js
@@ -0,0 +1,9 @@
+/* A variable is a label that references a value.
+Use the let keyword to declare a variable.
+An undefined variable is a variable that has been declared but not initialized while an undeclared variable is variable that has not been declared.
+Use the const keyword to define a readonly reference to a value.  */
+
+
+let text1="Utkarsh", text2=100;
+console.log(text1);
+console.log(text2);
