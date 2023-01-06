e4a06001a3e6cc4ed2940506cab34db7fcfa3c7c
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2023-01-06 23:52:16 +0530

    Create 2_Datatypes.js

---
+++ 2_Datatypes.js
@@ -0,0 +1,13 @@
+// Primitive datatypes : null,defined,boolean,number,string,symbol,bigint
+// complex datatype : object
+
+let counter = 120;
+console.log(typeof(counter));   //number
+
+counter = false; 
+console.log(typeof(counter));  //boolean
+
+counter = "Hi";
+console.log(typeof(counter));   //string
+
+
