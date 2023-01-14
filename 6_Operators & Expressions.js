451a94aea9af2ca7862dff06dc8a0b1c84558f06
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2023-01-14 12:06:51 +0530

    Create 6_Operators & Expressions.js

---
+++ 6_Operators & Expressions.js
@@ -0,0 +1,28 @@
+// Arithmetic Operators : +, -,*, /, **, %
+
+let a = 45
+let b = 4
+
+console.log("a+b = ", a+b)
+console.log("a-b = ", a-b)
+console.log("a*b = ", a*b)
+console.log("a/b = ", a/b)
+console.log("a%b = ", a%b)
+console.log("a^b = ", a**b)
+
+
+// Assignment Operators : =, +=, -=, *=, /=, %=, **=
+
+
+// Comparison Operators : ==, !=, ===, !==, >, <, >=, <=, ?
+
+let comp1 = 6
+let comp2 = "6"
+
+console.log("comp1 == comp2  is : ", comp1==comp2)
+console.log("comp1 != comp2  is : ", comp1!=comp2)
+console.log("comp1 === comp2  is : ", comp1===comp2)   //equal value and equal type
+console.log("comp1 !== comp2  is : ", comp1!==comp2)  // not equal value or not equal type
+
+// Logical Operators : &&, ||, !
+// Read truth table of Logical AND and Logical OR
