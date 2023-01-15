d6374effff0c557b36019d323bf991fb61629d1f
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-01-15 19:32:49 +0530

    Create 18_More array methods.js

---
+++ Chapter 5 (Arrays)/18_More array methods.js
@@ -0,0 +1,39 @@
+//length is not effected when delete property is applied
+
+let nums = [1,2,3,4,5,6]
+console.log(nums.length)  // output : 6
+delete nums[0]
+console.log(nums)  // output : [ <1 empty item>, 2, 3, 4, 5, 6 ]
+console.log(nums.length)   // output : 6
+
+console.log("\n")
+
+//concat returns a new array. Doesn't change the existing arrays.-----------------------------------------------------------------------
+
+let nums1=[11,12,13,14]
+let newArray = nums.concat(nums1)
+console.log(newArray)     // output : [ <1 empty item>, 2, 3, 4, 5, 6, 11, 12, 13, 14 ]
+
+
+//-----------------------------SORT------------------------------------------------------------------------------
+
+let arr=[-2,88,3,67,0]
+console.log(arr.sort())  //modifies the original array. It sorts array alphabetically.
+
+/* sort function takes an optional compare function. If the function is provided as the first argument then the sort function will consider these values (which are 
+returned from the compare function) as the basis of sorting.    */
+
+let compare = (a,b) =>
+{
+    return b-a
+}
+let arr=[-2,88,3,67,0]
+console.log(arr.sort(compare))
+
+// ------------------------------Splice and Slice--------------------------------------------------------------------------------------
+
+let container = [1,2,3,4,5]
+container.splice(2,3,1220,1450)//start from idx 2 and removes 3 elements and add 1220 and 1450.
+console.log(container)
+
+
