00bca3c2d343ed8dd580fb6c3ab8f11da82b159d
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-01-15 08:30:57 +0530

    Create 13_Strings.js

---
+++ Chapter 4/13_Strings.js
@@ -0,0 +1,15 @@
+let name = "Utkarsh"
+console.log(name.length)    // 7
+console.log(name[0])       // U
+
+let boy1 = "Yash"
+let boy2 = "Utkarsh"
+
+// template literals (use backtics) 
+console.log(`${boy2} is a friend of ${boy1}`)
+
+//Escape Sequence characters
+// There are 2 strings : bana and na
+
+let fruit = 'Bana\'na'
+console.log(fruit)
